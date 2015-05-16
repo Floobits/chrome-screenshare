7dcd0e362bffaed551460bf1526506bb012ec53e
Bjorn Tipling <bjorn@ambientchill.com>
2015-05-15 18:14:24 -0700

    Remove content script, move notes into bg

--- extension/js/background.js
+++ extension/js/background.js
@@ -26,7 +26,41 @@
   //    actions[msg.action].apply(actions, arguments);
   //  }
   //  return true;
+
   //});
+  //From content_script:
+  /*global chrome*/
+  //"use strict";
+  //var manifest = chrome.runtime.getManifest();
+  //
+  //function receiveMessage(event) {
+  //  switch (event.data.text) {
+  //    case "flooScreenDoWeHaveAnExtension":
+  //      postVersion();
+  //      break;
+  //    case "flooScreenShare":
+  //      chrome.runtime.sendMessage({action: "getScreen"}, function (id) {
+  //        window.postMessage({name: "flooScreenShareResponse", id: id}, "*");
+  //      });
+  //      break;
+  //    default:
+  //      console.log("starting import from chrome ext");
+  //      chrome.runtime.sendMessage({action: event.data.text});
+  //      break;
+  //  }
+  //}
+  //
+  //function postVersion () {
+  //  window.postMessage({name: "flooScreenHasExtension", version: manifest.version}, "*");
+  //}
+  //
+  //function main() {
+  //  window.addEventListener("message", receiveMessage, false);
+  //  postVersion();
+  //}
+  //
+  //main();
+
 
   chrome.runtime.onConnectExternal.addListener(function(port) {
     console.log("Got a connection", port);
