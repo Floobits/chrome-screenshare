c3ef17fa0b1982149b07e605ebe0e4594d73d0b3
Bjorn Tipling <bjorn@ambientchill.com>
2015-05-18 14:52:53 -0700

    Commit progress on file sync. #391

--- extension/js/background.js
+++ extension/js/background.js
@@ -1,71 +1,56 @@
 /*global chrome*/
 
 (function () {
+
+  var port,
+    manifest = chrome.runtime.getManifest();
+
   console.log("running bg");
-  //var actions = {
-  //  getScreen: function getScreen(msg, sender, cb) {
-  //    console.log("chooseDesktopMedia");
-  //    chrome.desktopCapture.chooseDesktopMedia(["screen", "window"], sender.tab, function (id) {
-  //      cb(id);
-  //    });
-  //  },
-  //  startImport: function startImport(msg, sender, cb) {
-  //    console.log("startImport!");
-  //    chrome.fileSystem.chooseEntry({
-  //      type: 'openDirectory'
-  //    }, function(dirEntry) {
-  //      console.log("got dirEntry", dirEntry);
-  //    });
-  //  }
-  //};
-  //
-  //chrome.runtime.onMessage.addListener(function (msg, sender, cb) {
-  //  console.log("got message");
-  //  console.log("msg data", msg, cb, arguments, actions, _.isFunction(actions[msg.action]));
-  //  if (msg && _.isFunction(actions[msg.action])) {
-  //    actions[msg.action].apply(actions, arguments);
-  //  }
-  //  return true;
 
-  //});
-  //From content_script:
-  /*global chrome*/
-  //"use strict";
-  //var manifest = chrome.runtime.getManifest();
-  //
-  //function receiveMessage(event) {
-  //  switch (event.data.text) {
-  //    case "flooScreenDoWeHaveAnExtension":
-  //      postVersion();
-  //      break;
-  //    case "flooScreenShare":
-  //      chrome.runtime.sendMessage({action: "getScreen"}, function (id) {
-  //        window.postMessage({name: "flooScreenShareResponse", id: id}, "*");
-  //      });
-  //      break;
-  //    default:
-  //      console.log("starting import from chrome ext");
-  //      chrome.runtime.sendMessage({action: event.data.text});
-  //      break;
-  //  }
-  //}
-  //
-  //function postVersion () {
-  //  window.postMessage({name: "flooScreenHasExtension", version: manifest.version}, "*");
-  //}
-  //
-  //function main() {
-  //  window.addEventListener("message", receiveMessage, false);
-  //  postVersion();
-  //}
-  //
-  //main();
+  function postMessage(data) {
+    if (!port) {
+      console.log("Tried to post without port");
+      return;
+    }
+    port.postMessage(data);
+  }
 
+  function getScreen() {
+    console.log("Getting screen");
+    var portURL = port.sender.url;
+      chrome.desktopCapture.chooseDesktopMedia(["screen", "window"], function (id) {
+        postMessage({name: "flooScreenShareResponse", id: id});
+      });
 
-  chrome.runtime.onConnectExternal.addListener(function(port) {
+  }
+  function startImport() {
+    console.log("startImport!");
+    chrome.fileSystem.chooseEntry({
+      type: 'openDirectory'
+    }, function(dirEntry) {
+      console.log("got dirEntry", dirEntry);
+    });
+  }
+
+  function postVersion () {
+    postMessage({name: "flooScreenHasExtension", version: manifest.version});
+  }
+
+  chrome.runtime.onConnectExternal.addListener(function(newPort) {
+    port = newPort;
     console.log("Got a connection", port);
     port.onMessage.addListener(function(msg) {
       console.log("Got a message", msg);
+      switch (msg.action) {
+        case "flooScreenDoWeHaveAnExtension":
+          postVersion();
+          break;
+        case "flooScreenShare":
+          getScreen();
+          break;
+        default:
+          break;
+      }
     });
   });
 }());
